298c9fcb4ed9a9bd093d53542c97b6f0541d61bc
JaneyChan <JaneChan.Hope@gmail.com>
2018-07-09 18:59:26 +0800

    【server】modify config.js

--- server/src/utils/config.js
+++ server/src/utils/config.js
@@ -1,14 +1,23 @@
+const db_host = process.env.DB_CONFIG_HOST || '127.0.0.1';
+const db_port = process.env.DB_CONFIG_PORT || 3306;
+const db_database = process.env.DB_CONFIG_DATABASE || 'me_blog';
+const db_user = process.env.DB_CONFIG_USER || 'root';
+const db_password = process.env.DB_CONFIG_PASSWORD || '123456';
+
+const jwt_secret = process.env.JWT_CONFIG_SECRET || 'secret';
+const jwt_exprisesIn = process.env.JWT_CONFIG_EXPRISESIN || 24*60*60*1000;
+
 module.exports = {
     mysqlDB : {
-        host: '127.0.0.1', // 数据库IP
-        port: 3306, // 数据库端口
-        database: 'me_blog', // 数据库名称
-        user: 'root', // 数据库用户名
-        password: '123456', // 数据库密码
+        host: db_host,
+        port: db_port,
+        database: db_database,
+        user: db_user,
+        password: db_password
     },
     jwt: {
-        secret: 'umeet',            
-        exprisesIn: 24*60*60*1000
+        secret: jwt_secret,            
+        exprisesIn: jwt_exprisesIn
     }
 }
 
